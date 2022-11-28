9d28016778a3821aa864b55a6345c9fd422367b1
Zhanarys17 <zhanarys.d7@gmail.com>
2022-11-28 21:48:13 +0600

    Added index.php file and made functions to work with Slim microframework!

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use RecipePhpMicroservice\controller\MailController;
+use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
+
+$dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__);
+$dotenv->load();
+
+$twig = Twig::create(__DIR__ . '/templates', ['cache' => false]);
+$app = AppFactory::create();
+$app->addErrorMiddleware(true, true, true);
+$app->add(TwigMiddleware::create($app, $twig));
+
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Hello world!");
+    return $response;
+});
+
+$app->get('/mails',MailController::class . ':index');
+$app->get('/mails/new',MailController::class . ':newMail');
+$app->post('/mails',MailController::class . ':create');
+
+$app->run();
